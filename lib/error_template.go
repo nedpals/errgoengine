1e944489871669b28ca568ea7339361e849cc604
Ned Palacios <npdoesmc@gmail.com>
2023-08-19 23:18:43 +0800

    add option to disable/add custom stack trace pattern

--- lib/error_template.go
+++ lib/error_template.go
@@ -5,32 +5,48 @@ import (
 )
 
 type ErrorTemplate struct {
-	Name           string
-	Pattern        string
-	OnGenExplainFn GenExplainFn
-	OnGenBugFixFn  GenBugFixFn
+	Name              string
+	Pattern           string
+	StackTracePattern string
+	OnGenExplainFn    GenExplainFn
+	OnGenBugFixFn     GenBugFixFn
 }
 
 type compiledErrorTemplate struct {
 	ErrorTemplate
-	Language *Language
-	Pattern  *regexp.Regexp
+	Language          *Language
+	Pattern           *regexp.Regexp
+	StackTracePattern *regexp.Regexp
 }
 
 type ErrorTemplates []*compiledErrorTemplate
 
 func (tmps *ErrorTemplates) Add(language *Language, template ErrorTemplate) *compiledErrorTemplate {
-	patternForCompile := "(?m)^" + template.Pattern + `(?P<stacktrace>(?:.|\s)*)$`
-	compiledPattern, err := regexp.Compile(patternForCompile)
+	var stackTracePattern *regexp.Regexp
+
+	patternForCompile := template.Pattern
+	if len(template.StackTracePattern) == 0 {
+		patternForCompile += `(?P<stacktrace>(?:.|\s)*)`
+	} else {
+		var err error
+		stackTracePattern, err = regexp.Compile(template.StackTracePattern)
+		if err != nil {
+			// TODO: should not panic!
+			panic(err)
+		}
+	}
+
+	compiledPattern, err := regexp.Compile("(?m)^" + patternForCompile + "$")
 	if err != nil {
 		// TODO: should not panic!
 		panic(err)
 	}
 
 	*tmps = append(*tmps, &compiledErrorTemplate{
-		ErrorTemplate: template,
-		Language:      language,
-		Pattern:       compiledPattern,
+		ErrorTemplate:     template,
+		Language:          language,
+		Pattern:           compiledPattern,
+		StackTracePattern: stackTracePattern,
 	})
 
 	return (*tmps)[len(*tmps)-1]
