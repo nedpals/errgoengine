5cc0437e70c414e0bb219a1527a2d43b73deb5c5
Ned Palacios <npdoesmc@gmail.com>
2023-08-31 22:01:26 +0800

    add ExpectNil

--- test_utils/test_utils.go
+++ test_utils/test_utils.go
@@ -1,6 +1,9 @@
 package testutils
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Equals[V comparable](tb testing.TB, got V, exp V) {
 	if got != exp {
@@ -14,4 +17,8 @@ func ExpectError(tb testing.TB, err error, exp string) {
 	}
 
 	Equals(tb, err.Error(), exp)
+}
+
+func ExpectNil(tb testing.TB, d any) {
+	Equals(tb, fmt.Sprintf("%v", d), "<nil>")
 }
