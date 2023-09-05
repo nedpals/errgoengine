28a73944d00568ff09701e987648cad6cb0a7a16
Ned Palacios <npdoesmc@gmail.com>
2023-09-05 22:06:54 +0800

    feat(test_utils): add new testing helpers

--- test_utils/test_utils.go
+++ test_utils/test_utils.go
@@ -11,6 +11,32 @@ func Equals[V comparable](tb testing.TB, got V, exp V) {
 	}
 }
 
+func EqualsMap[K comparable, V comparable](tb testing.TB, got map[K]V, exp map[K]V) {
+	if len(got) != len(exp) {
+		tb.Fatalf("\nexp (len %d): %v\ngot (len %d): %v", len(exp), exp, len(got), got)
+	}
+
+	for k, v := range exp {
+		if gotV, gotOk := got[k]; !gotOk {
+			tb.Fatalf("\nkey `%v` in got map not present", k)
+		} else if v != gotV {
+			tb.Fatalf("\nin got[%v]\nexp: %v\ngot: %v", k, v, gotV)
+		}
+	}
+}
+
+func EqualsList[V comparable](tb testing.TB, got []V, exp []V) {
+	if len(got) != len(exp) {
+		tb.Fatalf("\nexp (len %d): %v\ngot (len %d): %v", len(exp), exp, len(got), got)
+	}
+
+	for i, v := range exp {
+		if got[i] != v {
+			tb.Fatalf("\nin got[%v]\nexp: %v\ngot: %v", i, v, got[i])
+		}
+	}
+}
+
 func ExpectError(tb testing.TB, err error, exp string) {
 	if err == nil {
 		tb.Fatalf("expected error, got nil")
@@ -21,4 +47,10 @@ func ExpectError(tb testing.TB, err error, exp string) {
 
 func ExpectNil(tb testing.TB, d any) {
 	Equals(tb, fmt.Sprintf("%v", d), "<nil>")
+}
+
+func ExpectNoError(tb testing.TB, err error) {
+	if err != nil {
+		tb.Fatalf("expected none, got error\nerror: %s", err)
+	}
 }
