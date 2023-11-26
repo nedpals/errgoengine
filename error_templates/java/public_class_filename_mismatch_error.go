561382be0fa96fd2d001eedef00104fd9f11e2a6
Ned Palacios <npdoesmc@gmail.com>
2023-11-26 23:59:44 +0800

    feat: implement Java.PublicClassFilenameMismatchError

--- error_templates/java/public_class_filename_mismatch_error.go
+++ error_templates/java/public_class_filename_mismatch_error.go
@@ -1,24 +1,64 @@
 package java
 
 import (
+	"fmt"
 	"path/filepath"
+	"strings"
 
 	lib "github.com/nedpals/errgoengine"
 )
+
+type publicClassFilenameMismatchErrorCtx struct {
+	className             string
+	expectedClassName     string
+	actualClassFileName   string
+	expectedClassFilename string
+}
 
 var PublicClassFilenameMismatchError = lib.ErrorTemplate{
 	Name:              "PublicClassFilenameMismatchError",
 	Pattern:           comptimeErrorPattern(`class (?P<className>\S+) is public, should be declared in a file named (?P<classFileName>\S+\.java)`),
 	StackTracePattern: comptimeStackTracePattern,
+	OnAnalyzeErrorFn: func(cd *lib.ContextData, m *lib.MainError) {
+		className := cd.Variables["className"]
+		actualClassFilename := filepath.Base(cd.MainError.ErrorNode.DocumentPath)
+		m.Context = publicClassFilenameMismatchErrorCtx{
+			className:             className,
+			actualClassFileName:   actualClassFilename,                                  // the name from the filename
+			expectedClassName:     strings.Replace(actualClassFilename, ".java", "", 1), // the name from the filename without the extension
+			expectedClassFilename: className + ".java",                                  // the expected filename to be renamed
+		}
+
+		query := fmt.Sprintf(`(class_declaration name: (identifier) @class-name (#eq? @class-name "%s"))`, className)
+		rootNode := lib.WrapNode(m.Nearest.Doc, m.Nearest.Doc.Tree.RootNode())
+
+		lib.QueryNode(rootNode, strings.NewReader(query), func(ctx lib.QueryNodeCtx) bool {
+			match := ctx.Cursor.FilterPredicates(ctx.Match, []byte(m.Nearest.Doc.Contents))
+			for _, c := range match.Captures {
+				node := lib.WrapNode(m.Nearest.Doc, c.Node)
+				m.Nearest = node
+				return false
+			}
+			return true
+		})
+	},
 	OnGenExplainFn: func(cd *lib.ContextData, gen *lib.ExplainGenerator) {
-		gen.Add(
-			`Public class "%s" does not match the name of the file which is "%s". Rename it to "%s"`,
-			cd.Variables["className"],
-			filepath.Base(cd.MainError.ErrorNode.DocumentPath),
-			cd.Variables["classFileName"],
-		)
+		gen.Add(`This error occurs because the name of the Java file does not match the name of the public class within it.`)
 	},
 	OnGenBugFixFn: func(cd *lib.ContextData, gen *lib.BugFixGenerator) {
+		ctx := cd.MainError.Context.(publicClassFilenameMismatchErrorCtx)
 
+		gen.Add("Rename your file", func(s *lib.BugFixSuggestion) {
+			s.AddStep("Rename the file to \"%s\" to match the class", ctx.expectedClassFilename)
+		})
+
+		gen.Add("Rename the public class", func(s *lib.BugFixSuggestion) {
+			s.AddStep("The filename should match the name of the public class in the file. To resolve this, change the class name to match the filename.").
+				AddFix(lib.FixSuggestion{
+					NewText:       ctx.expectedClassName,
+					StartPosition: cd.MainError.Nearest.StartPosition(),
+					EndPosition:   cd.MainError.Nearest.EndPosition(),
+				})
+		})
 	},
 }
