fbdf3be29fa3481815bd6e815fe4a55553dd5dd9
Ned Palacios <npdoesmc@gmail.com>
2024-01-27 17:49:48 +0800

    feat: add complex test case for Java.IdentifierExpectedError

---
+++ error_templates/java/test_files/identifier_expected_error_complex/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    int x = 5;
+
+    System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        System.out.println("Inside the main method.");
+    }
+}
